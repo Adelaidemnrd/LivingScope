30bebc489218a5055fa74b61bacce291fa1dd331
Adelaidemnrd <115484344+Adelaidemnrd@users.noreply.github.com>
2026-01-21 08:51:53 +0100

    Update analyze.js

--- analyze.js
+++ analyze.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+  res.status(200).json({ message: "Hello from Vercel API" });
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
